9142130e35229df4f4e7e78b2dba0cb2a8567bb0
Paulo da Silva Pereira <paulo.s.p@live.com>
2023-08-14 14:55:36 -0300

    segundo commit orgam - curso alura

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,17 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+    console.log(props.item)
+
+    return (
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select required = {props.required}>
+                {props.itens.map(item => <option key={item}>{item}</option>)}
+            </select>
+        </div>
+    )
+
+}
+
+export default ListaSuspensa
