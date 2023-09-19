ef1292e694ecd20a91b267aa2af2c69c69ad1a08
Paulo Pereira <paulo.spereira@dataprev.gov.br>
2023-09-19 16:11:34 -0300

    organ curso Alura - Finalizado

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,12 +1,11 @@
 import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
-    console.log(props.item)
-
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select required = {props.required}>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+                <option value=""></option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
